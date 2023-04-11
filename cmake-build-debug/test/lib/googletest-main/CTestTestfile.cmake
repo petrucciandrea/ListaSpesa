42709a3276d16476fb456df57c433c0c44432abb
andreapetrucci <a.petru01@hotmail.com>
2023-04-11 14:09:40 +0200

    ignore

--- cmake-build-debug/test/lib/googletest-main/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/andreapetrucci/CLionProjects/ListaSpesa/test/lib/googletest-main
-# Build directory: /Users/andreapetrucci/CLionProjects/ListaSpesa/cmake-build-debug/test/lib/googletest-main
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
